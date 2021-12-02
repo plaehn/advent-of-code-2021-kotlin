28628646738dbd6076eae742618c2762fa3e4507
plaehn.oliver <plaehn.oliver@swm.de>
2021-12-02 16:16:42 +0100

    Solution for day 1 part 2

--- src/main/kotlin/org/plaehn/adventofcode/day1/SonarSweep.kt
+++ src/main/kotlin/org/plaehn/adventofcode/day1/SonarSweep.kt
@@ -1,20 +1,28 @@
 package org.plaehn.adventofcode.day1
 
-class SonarSweep(private val measurements: List<Int>) {
+object SonarSweep {
 
-    fun countIncreasingMeasurements() =
-        measurements
-            .fold(Accumulator()) { accumulator: Accumulator, measurement: Int ->
-                accumulator.accumulateNext(measurement)
+    fun countIncreasingMeasurementsOfSlidingWindows(values: List<Int>) =
+        countIncreasingMeasurements(values = sumSlidingWindows(values))
+
+    private fun sumSlidingWindows(values: List<Int>) =
+        values
+            .windowed(size = 3)
+            .map { it.sum() }
+
+    fun countIncreasingMeasurements(values: List<Int>) =
+        values
+            .fold(Accumulator()) { accumulator: Accumulator, nextValue: Int ->
+                accumulator.accumulate(nextValue)
             }.count
 
     private data class Accumulator(val count: Int = 0, val previous: Int = -1) {
 
-        fun accumulateNext(measurement: Int) =
-            Accumulator(computeNewCount(measurement), measurement)
+        fun accumulate(nextValue: Int) =
+            Accumulator(computeNewCount(nextValue), nextValue)
 
-        private fun computeNewCount(measurement: Int) =
-            if (hasPrevious() && measurement > previous)
+        private fun computeNewCount(nextValue: Int) =
+            if (hasPrevious() && nextValue > previous)
                 count + 1
             else
                 count
--- src/test/kotlin/org/plaehn/adventofcode/day1/SonarSweepTest.kt
+++ src/test/kotlin/org/plaehn/adventofcode/day1/SonarSweepTest.kt
@@ -10,8 +10,7 @@ internal class SonarSweepTest {
     fun `Count measurements larger than previous on test input`() {
         val measurements = readInput("test_input.txt")
 
-        val sonarSweep = SonarSweep(measurements)
-        val numberOfIncreasingMeasurements = sonarSweep.countIncreasingMeasurements()
+        val numberOfIncreasingMeasurements = SonarSweep.countIncreasingMeasurements(measurements)
 
         assertThat(numberOfIncreasingMeasurements).isEqualTo(7)
     }
@@ -20,10 +19,27 @@ internal class SonarSweepTest {
     fun `Count measurements larger than previous on puzzle input`() {
         val measurements = readInput("puzzle_input.txt")
 
-        val sonarSweep = SonarSweep(measurements)
-        val numberOfIncreasingMeasurements = sonarSweep.countIncreasingMeasurements()
+        val numberOfIncreasingMeasurements = SonarSweep.countIncreasingMeasurements(measurements)
 
         assertThat(numberOfIncreasingMeasurements).isEqualTo(1448)
+    }
+
+    @Test
+    fun `Count measurements larger than previous using windows of size 3 on test input`() {
+        val measurements = readInput("test_input.txt")
+
+        val numberOfIncreasingMeasurements = SonarSweep.countIncreasingMeasurementsOfSlidingWindows(measurements)
+
+        assertThat(numberOfIncreasingMeasurements).isEqualTo(5)
+    }
+
+    @Test
+    fun `Count measurements larger than previous using windows of size 3 on puzzle input`() {
+        val measurements = readInput("puzzle_input.txt")
+
+        val numberOfIncreasingMeasurements = SonarSweep.countIncreasingMeasurementsOfSlidingWindows(measurements)
+
+        assertThat(numberOfIncreasingMeasurements).isEqualTo(1471)
     }
 
     private fun readInput(ressourceName: String): List<Int> =
