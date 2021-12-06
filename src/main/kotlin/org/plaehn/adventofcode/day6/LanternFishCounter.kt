583b6e7246b3f6f431262d941cf5f6e923338a80
plaehn.oliver <plaehn.oliver@swm.de>
2021-12-06 17:17:31 +0100

    Solution for day 6 part 2

--- src/main/kotlin/org/plaehn/adventofcode/day6/LanternFishCounter.kt
+++ src/main/kotlin/org/plaehn/adventofcode/day6/LanternFishCounter.kt
@@ -1,28 +1,32 @@
 package org.plaehn.adventofcode.day6
 
 class LanternFishCounter(
-    private val initialLanternFishes: List<Int>
+    initialLanternFishes: List<Int>
 ) {
 
-    fun computeNumberOfLanternFishesAfter(days: Int): Int {
-        var fishes = initialLanternFishes
-        repeat(days) {
-            fishes = computeNext(fishes)
+    private val age2Count = Array(9) { 0L }
+
+    init {
+        initialLanternFishes.forEach {
+            age2Count[it]++
         }
-        return fishes.size
     }
 
-    private fun computeNext(fishes: List<Int>) =
-        sequence {
-            fishes.forEach { age ->
-                if (age == 0) {
-                    yield(6)
-                    yield(8)
-                } else {
-                    yield(age - 1)
-                }
-            }
-        }.toList()
+    fun computeNumberOfLanternFishesAfter(days: Int): Long {
+        repeat(days) {
+            computeNext()
+        }
+        return age2Count.sum()
+    }
+
+    private fun computeNext() {
+        val spawnCount = age2Count[0]
+        for (age in 0..7) {
+            age2Count[age] = age2Count[age + 1]
+        }
+        age2Count[6] = spawnCount + age2Count[6]
+        age2Count[8] = spawnCount
+    }
 
     companion object {
         fun fromInputString(input: String) =
--- src/test/kotlin/org/plaehn/adventofcode/day6/LanternFishCounterTest.kt
+++ src/test/kotlin/org/plaehn/adventofcode/day6/LanternFishCounterTest.kt
@@ -12,7 +12,7 @@ internal class LanternFishCounterTest {
         val input = this::class.java.slurp("test_input.txt")
 
         val lanternFishCounter = LanternFishCounter.fromInputString(input)
-        val count = lanternFishCounter.computeNumberOfLanternFishesAfter(80)
+        val count = lanternFishCounter.computeNumberOfLanternFishesAfter(days = 80)
 
         assertThat(count).isEqualTo(5934)
     }
@@ -22,9 +22,29 @@ internal class LanternFishCounterTest {
         val input = this::class.java.slurp("puzzle_input.txt")
 
         val lanternFishCounter = LanternFishCounter.fromInputString(input)
-        val count = lanternFishCounter.computeNumberOfLanternFishesAfter(80)
+        val count = lanternFishCounter.computeNumberOfLanternFishesAfter(days = 80)
 
         assertThat(count).isEqualTo(377263)
+    }
+
+    @Test
+    fun `Compute number of lantern fished after 256 days on test input`() {
+        val input = this::class.java.slurp("test_input.txt")
+
+        val lanternFishCounter = LanternFishCounter.fromInputString(input)
+        val count = lanternFishCounter.computeNumberOfLanternFishesAfter(days = 256)
+
+        assertThat(count).isEqualTo(26984457539)
+    }
+
+    @Test
+    fun `Compute number of lantern fished after 256 days on puzzle input`() {
+        val input = this::class.java.slurp("puzzle_input.txt")
+
+        val lanternFishCounter = LanternFishCounter.fromInputString(input)
+        val count = lanternFishCounter.computeNumberOfLanternFishesAfter(days = 256)
+
+        assertThat(count).isEqualTo(1695929023803)
     }
 }
 
