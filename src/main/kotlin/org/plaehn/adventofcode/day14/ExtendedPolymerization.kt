36cf6e35cf2491b2fb159925a4fe62f62ebb72fc
plaehn.oliver <plaehn.oliver@swm.de>
2021-12-14 15:10:01 +0100

    Day 14, part 2

--- src/main/kotlin/org/plaehn/adventofcode/day14/ExtendedPolymerization.kt
+++ src/main/kotlin/org/plaehn/adventofcode/day14/ExtendedPolymerization.kt
@@ -4,34 +4,65 @@ import org.plaehn.adventofcode.common.groupByBlankLines
 
 class ExtendedPolymerization(
     private val polymerTemplate: String,
-    private val insertionRules: Map<String, String>
+    insertionRules: Map<String, String>
 ) {
+    private val transformationRules =
+        insertionRules
+            .map { it.key to Pair(it.key[0] + it.value, it.value + it.key[1]) }
+            .toMap()
 
-    fun computeSolution(): Int {
-        val elementCounts = (1..10)
-            .fold(polymerTemplate) { polymerTemplate, _ -> step(polymerTemplate) }
+    private val initialPairCount =
+        polymerTemplate
+            .zipWithNext()
+            .map { it.toList().joinToString("") }
             .groupingBy { it }
             .eachCount()
+            .mapValues { it.value.toLong() }
 
-        val mostCommon = elementCounts.maxOf { it.value }
-        val leastCommon = elementCounts.minOf { it.value }
+    fun computeSolution(numberOfSteps: Int): Long {
+        val finalPairCount = (1..numberOfSteps).fold(initialPairCount) { pairCount, _ -> step(pairCount) }
+
+        val charCount: MutableMap<Char, Long> =
+            finalPairCount
+                .entries
+                .map { listOf(it.key[0] to it.value, it.key[1] to it.value) }
+                .flatten()
+                .groupBy { it.first }
+                .map { entry -> entry.key to entry.value.sumOf { it.second } }
+                .toMap().toMutableMap()
+
+        charCount[polymerTemplate.first()] = (charCount[polymerTemplate.first()]!! - 1L)
+        charCount[polymerTemplate.last()] = (charCount[polymerTemplate.last()]!! + 1L)
+
+        val mostCommon = charCount.maxOf { it.value / 2 }
+        val leastCommon = charCount.minOf { it.value / 2 }
 
         return mostCommon - leastCommon
     }
 
-    private fun step(polymerTemplate: String) =
-        polymerTemplate
-            .zipWithNext()
-            .map { it.toList().joinToString("") }
-            .joinToString("") { it[0] + insertionRules[it].toString() } + polymerTemplate.last()
+    private fun step(pairCount: Map<String, Long>): Map<String, Long> {
+        val oldEntries: List<Pair<String, Long>> = pairCount.toList()
+        val updates: List<Pair<String, Long>> =
+            pairCount.map { entry ->
+                val count = entry.value
+                transformationRules[entry.key]!!.toList().map { it to count } + (entry.key to -count)
+            }.flatten()
+
+        val newEntries: Map<String, Long> =
+            (oldEntries + updates)
+                .groupBy { it.first }
+                .map { entry -> entry.key to entry.value.sumOf { it.second } }
+                .toMap()
+        return newEntries
+    }
 
     companion object {
         fun fromInput(input: String): ExtendedPolymerization {
             val (polymerTemplate, pairInsertions) = input.groupByBlankLines()
-            val insertionRules = pairInsertions.lines().map {
+            val insertionRules = pairInsertions.lines().associate {
                 val (from, to) = it.split(" -> ")
                 from to to
-            }.toMap()
+            }
             return ExtendedPolymerization(polymerTemplate, insertionRules)
         }
     }
--- src/test/kotlin/org/plaehn/adventofcode/day14/ExtendedPolymerizationTest.kt
+++ src/test/kotlin/org/plaehn/adventofcode/day14/ExtendedPolymerizationTest.kt
@@ -5,27 +5,50 @@ import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
 import org.plaehn.adventofcode.common.slurp
 
+@ExperimentalStdlibApi
 internal class ExtendedPolymerizationTest {
 
     @Test
-    fun `Compute solution for test input`() {
+    fun `Compute solution after 10 steps for test input`() {
         val input = this::class.java.slurp("test_input.txt")
 
         val polymerization = ExtendedPolymerization.fromInput(input)
 
-        val solution = polymerization.computeSolution()
+        val solution = polymerization.computeSolution(10)
 
         assertThat(solution).isEqualTo(1588)
     }
 
     @Test
-    fun `Compute solution for puzzle input`() {
+    fun `Compute solution after 10 steps for puzzle input`() {
         val input = this::class.java.slurp("puzzle_input.txt")
 
         val polymerization = ExtendedPolymerization.fromInput(input)
 
-        val solution = polymerization.computeSolution()
+        val solution = polymerization.computeSolution(10)
 
         assertThat(solution).isEqualTo(3406)
+    }
+
+    @Test
+    fun `Compute solution after 40 steps for test input`() {
+        val input = this::class.java.slurp("test_input.txt")
+
+        val polymerization = ExtendedPolymerization.fromInput(input)
+
+        val solution = polymerization.computeSolution(40)
+
+        assertThat(solution).isEqualTo(2188189693529)
+    }
+
+    @Test
+    fun `Compute solution after 40 steps for puzzle input`() {
+        val input = this::class.java.slurp("puzzle_input.txt")
+
+        val polymerization = ExtendedPolymerization.fromInput(input)
+
+        val solution = polymerization.computeSolution(40)
+
+        assertThat(solution).isEqualTo(3941782230241)
     }
 }
