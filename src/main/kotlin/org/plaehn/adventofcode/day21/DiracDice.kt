cadf0a25d931e7275127bdaee117b6d600354bb2
Oliver Plaehn <oliver.plaehn@gmail.com>
2022-01-06 10:41:15 +0000

    Day 21, part 2
    again, solution courtesy of Todd Ginsberg

--- src/main/kotlin/org/plaehn/adventofcode/day21/DiracDice.kt
+++ src/main/kotlin/org/plaehn/adventofcode/day21/DiracDice.kt
@@ -1,5 +1,7 @@
 package org.plaehn.adventofcode.day21
 
+import com.google.common.collect.Sets
+
 class DiracDice(start1: Int, start2: Int) {
 
     private val initialGameState = GameState(PlayerState(start1), PlayerState(start2))
@@ -13,9 +15,30 @@ class DiracDice(start1: Int, start2: Int) {
         return gameState.minScore() * die.numberOfRolls
     }
 
-    fun solvePart2(): Long {
-        TODO()
+    fun solvePart2() = playWithQuantumDie().max()
+
+    private val quantumDieRollToFrequencyMap: Map<Int, Int> by lazy {
+        val oneRoll = setOf(1, 2, 3)
+        Sets.cartesianProduct(oneRoll, oneRoll, oneRoll)
+            .map { threeRolls -> threeRolls.sum() }
+            .groupingBy { it }
+            .eachCount()
     }
+
+    private val stateMemory: MutableMap<GameState, WinCounts> = mutableMapOf()
+
+    private fun playWithQuantumDie(state: GameState = initialGameState): WinCounts =
+        when {
+            state.isWinner(scoreNeeded = 21) ->
+                if (state.player1.score > state.player2.score) WinCounts(1, 0) else WinCounts(0, 1)
+            state in stateMemory ->
+                stateMemory.getValue(state)
+            else ->
+                quantumDieRollToFrequencyMap.map { (roll, frequency) ->
+                    playWithQuantumDie(state.next(roll)) * frequency
+                }.reduce { a, b -> a + b }.also { stateMemory[state] = it }
+
+        }
 
     companion object {
         fun fromInputLines(inputLines: List<String>) =
@@ -29,10 +52,10 @@ data class GameState(
     val player1Turn: Boolean = true
 ) {
 
-    fun next(die: Int) =
+    fun next(roll: Int) =
         GameState(
-            if (player1Turn) player1.advance(die) else player1,
-            if (!player1Turn) player2.advance(die) else player2,
+            if (player1Turn) player1.advance(roll) else player1,
+            if (!player1Turn) player2.advance(roll) else player2,
             player1Turn = !player1Turn
         )
 
@@ -58,4 +81,13 @@ data class DeterministicDie(private var value: Int = 0, var numberOfRolls: Int =
         value += 3
         return 3 * value - 3
     }
+}
+
+data class WinCounts(val player1: Long = 0, val player2: Long = 0) {
+
+    operator fun plus(other: WinCounts) = WinCounts(player1 + other.player1, player2 + other.player2)
+
+    operator fun times(multiplier: Int) = WinCounts(player1 * multiplier, player2 * multiplier)
+
+    fun max() = maxOf(player1, player2)
 }
--- src/test/kotlin/org/plaehn/adventofcode/day21/DiracDiceTest.kt
+++ src/test/kotlin/org/plaehn/adventofcode/day21/DiracDiceTest.kt
@@ -2,7 +2,6 @@ package org.plaehn.adventofcode.day21
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.plaehn.adventofcode.common.readLines
 
@@ -26,7 +25,6 @@ internal class DiracDiceTest {
         assertThat(diracDice.solvePart1()).isEqualTo(518418)
     }
 
-    @Disabled
     @Test
     fun `Solve part 2 for test input`() {
         val inputLines = this::class.java.readLines("test_input.txt")
@@ -36,13 +34,12 @@ internal class DiracDiceTest {
         assertThat(diracDice.solvePart2()).isEqualTo(444356092776315)
     }
 
-    @Disabled
     @Test
     fun `Solve part 2 for puzzle input`() {
         val inputLines = this::class.java.readLines("puzzle_input.txt")
 
         val diracDice = DiracDice.fromInputLines(inputLines)
 
-        assertThat(diracDice.solvePart2()).isEqualTo(0)
+        assertThat(diracDice.solvePart2()).isEqualTo(116741133558209)
     }
 }
